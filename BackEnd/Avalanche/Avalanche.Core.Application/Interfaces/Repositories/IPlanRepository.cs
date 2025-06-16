69c31b3f25f95e1023dd913756904c92b9a299f1
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-06-16 16:58:05 -0400

    Changing repositories name

---
+++ BackEnd/Avalanche/Avalanche.Core.Application/Interfaces/Repositories/IPlanRepository.cs
@@ -0,0 +1,9 @@
+﻿using Avalanche.Core.Domain.Entities;
+
+namespace Avalanche.Core.Application.Interfaces.Repositories
+{
+    public interface IPlanRepository : IGenericRepository<Plan>
+    {
+        Task<Plan> GetByNameAsync(string name);
+    }
+}
