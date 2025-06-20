1c7966c611e84166e85699186b9a61214f88732f
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-06-19 22:12:56 -0400

    Adding unique index

---
+++ BackEnd/Avalanche/Avalanche.Infrastructure.Persistence/Migrations/20250620020525_AddingUniqueIndex.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Avalanche.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Policies_Number",
+                table: "Policies",
+                column: "Number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_DocumentNumber",
+                table: "Clients",
+                column: "DocumentNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Affiliates_DocumentNumber",
+                table: "Affiliates",
+                column: "DocumentNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Policies_Number",
+                table: "Policies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_DocumentNumber",
+                table: "Clients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Affiliates_DocumentNumber",
+                table: "Affiliates");
+        }
+    }
+}
