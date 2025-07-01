9c5fd62710f7d9182bdd0738493125593b098b74
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-07-01 00:02:10 -0400

    Add check authorization endpoint for hospital integration
    Introduced CheckAuthorizationQuery and its response model to allow hospitals to query the status of authorization requests. Added a new GET endpoint in HospitalIntegrationController to handle these queries, returning appropriate responses for success, not found, unauthorized, and server errors.

--- BackEnd/Avalanche/Avalanche.Interface.BusinessApi/Controllers/v1/HospitalIntegrationController.cs
+++ BackEnd/Avalanche/Avalanche.Interface.BusinessApi/Controllers/v1/HospitalIntegrationController.cs
@@ -2,13 +2,13 @@
 using Avalanche.Core.Application.Dtos.Common;
 using Avalanche.Core.Application.Dtos.HospitalIntegration;
 using Avalanche.Core.Application.Features.HospitalIntegration.Command.PayBills;
+using Avalanche.Core.Application.Features.HospitalIntegration.Queries.CheckAuthorization;
 using Avalanche.Core.Application.Features.HospitalIntegration.Queries.ValidateAffiliate;
 using Avalanche.Core.Application.Helpers;
 using Avalanche.Interface.BusinessAPI.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Avalanche.Interface.BusinessApi.Controllers.v1
 {
@@ -55,6 +55,33 @@ namespace Avalanche.Interface.BusinessApi.Controllers.v1
             }
             catch (Exception e)
             {
+                return StatusCode(StatusCodes.Status500InternalServerError, ErrorMapperHelper.Error(ErrorMessages.InternalServer, e.Message));
+            }
+        }
+
+        [Authorize(Roles = "Administrator, Guest")]
+        [HttpGet("check-authorization/{authorizationNumber}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CheckAuthorizationQueryResponse))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorDTO))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ErrorDTO))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorDTO))]
+        [SwaggerOperation(
+           Summary = "Consultar solicitudes",
+           Description = "Permite a los hospitales consultar el estado de las solicitudes"
+        )]
+        public async Task<IActionResult> CheckAuthorization(string authorizationNumber)
+        {
+            try
+            {
+                var result = await Mediator.Send(new CheckAuthorizationQuery() { AuthorizationNumber = authorizationNumber });
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                if (e.Message == ErrorMessages.NotFound)
+                    return NotFound(ErrorMapperHelper.Error(ErrorMessages.NotFound, "No existe una solicitud con ese identificador"));
+
                 return StatusCode(StatusCodes.Status500InternalServerError, ErrorMapperHelper.Error(ErrorMessages.InternalServer, e.Message));
             }
         }
