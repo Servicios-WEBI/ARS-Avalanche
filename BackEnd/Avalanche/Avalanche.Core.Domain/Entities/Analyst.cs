d90e32c638b223911be3fc1f17debf9bbd50a6b9
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-07-09 23:24:08 -0400

    Add Analyst entity and link to Authorization
    Introduces the Analyst entity and establishes a relationship between Analyst and Authorization. Updates the ApplicationContext and database schema to support the new entity and its association, including migration files and model snapshot updates.

---
+++ BackEnd/Avalanche/Avalanche.Core.Domain/Entities/Analyst.cs
@@ -0,0 +1,12 @@
+﻿using Avalanche.Core.Domain.Common;
+
+namespace Avalanche.Core.Domain.Entities
+{
+    public class Analyst : AuditableBaseEntity
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public List<Authorization> Authorizations { get; set; }
+    }
+}
