bd02bbaa3da826bf58b35092e1e0d04e3b2ea827
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-07-13 01:05:35 -0400

    Add GetById authorization query and endpoint
    Introduced GetByIdAuthorizationQuery and its response model to retrieve detailed authorization information by ID. Updated AuthorizationController to include a new GET endpoint for fetching authorization details, and expanded role access to include 'Analyst'. Also added 'AssignedAnalyst' to GetAll response and query.

---
+++ BackEnd/Avalanche/Avalanche.Core.Application/Features/Authorization/Queries/GetById/GetAllAuthorizationQueryResponse.cs
@@ -0,0 +1,68 @@
+﻿using Avalanche.Core.Application.Dtos.HospitalIntegration;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Avalanche.Core.Application.Features.Authorization.Queries.GetById
+{
+    public class GetByIdAuthorizationQueryResponse
+    {
+        [SwaggerSchema(Description = "Autorización")]
+        public GetByIdAuthorizationQueryResponseChild Authorization { get; set; }
+    }
+
+    public class GetByIdAuthorizationQueryResponseChild
+    {
+        [SwaggerSchema(Description = "Número de la solicitud.")]
+        public string Id { get; set; }
+
+        [SwaggerSchema(Description = "Nombre del afiliado")]
+        public string Affiliate { get; set; }
+
+        [SwaggerSchema(Description = "Id del afiliado")]
+        public string AffiliateId { get; set; }
+
+        [SwaggerSchema(Description = "Tipo de documento del afiliado.")]
+        public string DocumentType { get; set; }
+
+        [SwaggerSchema(Description = "Número del documento del afiliado.")]
+        public string DocumentNumber { get; set; }
+
+        [SwaggerSchema(Description = "Número de la póliza asociada a la solicitud.")]
+        public string? PolicyNumber { get; set; }
+
+        [SwaggerSchema(Description = "Id de la póliza asociada a la solicitud.")]
+        public string? PolicyId { get; set; }
+
+        [SwaggerSchema(Description = "Plan del afiliado")]
+        public string Plan { get; set; }
+
+        [SwaggerSchema(Description = "Tipo de autorización solicitada.")]
+        public string AuthorizationType { get; set; }
+
+        [SwaggerSchema(Description = "Id del tipo de autorización solicitada.")]
+        public string AuthorizationTypeId { get; set; }
+
+        [SwaggerSchema(Description = "Monto solicitado en la solicitud.")]
+        public double ApplicationAmount { get; set; }
+
+        [SwaggerSchema(Description = "Monto aprobado para la solicitud.")]
+        public double? ApprovedAmount { get; set; }
+
+        [SwaggerSchema(Description = "Estado actual de la solicitud.")]
+        public string Status { get; set; }
+
+        [SwaggerSchema(Description = "Id del estado actual de la solicitud.")]
+        public string StatusId { get; set; }
+
+        [SwaggerSchema(Description = "Fecha en que se realizó la solicitud de autorización.")]
+        public DateOnly ApplicationDate { get; set; }
+
+        [SwaggerSchema(Description = "Analista asignado")]
+        public string AssignedAnalyst { get; set; }
+
+        [SwaggerSchema(Description = "Hospital que realizó la solicitud.")]
+        public string Hospital { get; set; }
+
+        [SwaggerSchema(Description = "Id del Hospital que realizó la solicitud.")]
+        public string HospitalId { get; set; }
+    }
+}
---
+++ BackEnd/Avalanche/Avalanche.Core.Application/Features/Authorization/Queries/GetById/GetByIdAuthorizationQuery.cs
@@ -0,0 +1,81 @@
+﻿using Avalanche.Core.Application.Constants;
+using Avalanche.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System.Linq.Expressions;
+
+namespace Avalanche.Core.Application.Features.Authorization.Queries.GetById
+{
+    public class GetByIdAuthorizationQuery : IRequest<GetByIdAuthorizationQueryResponse>
+    {
+        public string Id { get; set; }
+    }
+
+    public class GetByIdAuthorizationQueryHandler : IRequestHandler<GetByIdAuthorizationQuery, GetByIdAuthorizationQueryResponse>
+    {
+        private readonly IAuthorizationRepository _authorizationRepository;
+        private readonly IDocumentTypeRepository _documentTypeRepository;
+        private readonly IPlanRepository _planRepository;
+
+        public GetByIdAuthorizationQueryHandler(IAuthorizationRepository authorizationRepository, IDocumentTypeRepository documentTypeRepository,
+            IPlanRepository planRepository)
+        {
+            _authorizationRepository = authorizationRepository;
+            _documentTypeRepository = documentTypeRepository;
+            _planRepository = planRepository;
+        }
+
+        public async Task<GetByIdAuthorizationQueryResponse> Handle(GetByIdAuthorizationQuery query, CancellationToken cancellationToken)
+        {
+            try
+            {
+                GetByIdAuthorizationQueryResponse result = new();
+
+                var entity = await _authorizationRepository.GetByIdWithIncludeAsync(a => a.Id == query.Id, new List<Expression<Func<Domain.Entities.Authorization, object>>>
+                {
+                    m => m.Affiliate,
+                    m => m.Analyst,
+                    m => m.AuthorizationType,
+                    m => m.Hospital,
+                    m => m.Policy,
+                    m => m.Status
+                });
+
+                if (entity == null)
+                {
+                    throw new Exception(ErrorMessages.NotFound);
+                }
+
+                var document = await _documentTypeRepository.GetByIdAsync(entity.Affiliate.DocumentTypeId);
+                var plan = await _planRepository.GetByIdAsync(entity.Policy.PlanId);
+
+                result.Authorization = new GetByIdAuthorizationQueryResponseChild()
+                {
+                    Id = entity.Id,
+                    ApplicationDate = entity.ApplicationDate,
+                    ApplicationAmount = entity.ApplicationAmount,
+                    ApprovedAmount = entity.ApprovedAmount,
+                    Affiliate = entity.Affiliate.FirstName + " " + entity.Affiliate.LastName,
+                    AffiliateId = entity.AffiliateId,
+                    Status = entity.Status.Name,
+                    StatusId = entity.StatusId,
+                    AuthorizationType = entity.AuthorizationType.Name,
+                    AuthorizationTypeId = entity.AuthorizationTypeId,
+                    DocumentType = document.Name,
+                    DocumentNumber = entity.Affiliate.DocumentNumber,
+                    Hospital = entity.Hospital.Name,
+                    HospitalId = entity.HospitalId,
+                    PolicyNumber = entity.Policy.Number,
+                    PolicyId = entity.PolicyId,
+                    AssignedAnalyst = entity.Analyst.FullName,
+                    Plan = plan.Name
+                };
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
